0cfe335b7897da9dd82606c5abbe6c6e4be64f4c
Celso PALMEIRA <celso.palmeira@shares.io>
2025-10-21 15:06:20 +0100

    feat: update install script for installing zsh and setting up gitconfig
    Squashed commit of the following:
    
    commit edfca8017a87f3ccd1a95898be7a608403ed98e6
    Author: Celso PALMEIRA <celso.palmeira@shares.io>
    Date:   Tue Oct 21 15:05:53 2025 +0100
    
        chore: set gitconfig
    
    commit 35a61f161100ef39e82d813ec4ef4e948647400c
    Author: Celso PALMEIRA <celso.palmeira@shares.io>
    Date:   Tue Oct 21 14:34:21 2025 +0100
    
        chore: installs zsh before installing oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,3 +1,40 @@
+#!/bin/bash
+
+# This function checks if zsh is installed
+check_zsh_installed() {
+    if command -v zsh &> /dev/null
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Check if zsh is installed, if not install it
+if ! check_zsh_installed
+then
+    echo "zsh could not be found, installing zsh..."
+    sudo apt update
+    sudo apt install -y zsh
+fi
+
+# Check again if zsh is installed
+if ! check_zsh_installed
+then
+    echo "Failed to install zsh. Exiting."
+    exit 1
+fi
+
+sudo chsh -s $(which zsh)
+
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+# Merge .gitconfig if it exists
+if [ -f "$HOME/.gitconfig" ]; then
+    echo "Merging existing .gitconfig with new configuration..."
+    
+    # Add blank line before appending new config
+    echo "" >> "$HOME/.gitconfig"
+    cat .gitconfig >> "$HOME/.gitconfig"
+fi
