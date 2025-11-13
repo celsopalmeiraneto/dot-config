b477b1b936e4b9d8bd94ab66ea457ee7296b0f9d
Celso Palmeira Neto <neto@celsoneto.com.br>
2025-11-13 09:05:56 +0000

    feat: install btop

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo apt-get update
+
+# Install btop
+sudo apt-get install btop
+
 
