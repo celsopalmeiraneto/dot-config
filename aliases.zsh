1c219df456fd0d30105bc6aebe49e7ca926af689
Celso Palmeira Neto <neto@celsoneto.com.br>
2026-01-29 11:44:38 +0000

    chore: add new aliases for git and ls commands

--- aliases.zsh
+++ aliases.zsh
@@ -1,1 +1,3 @@
 alias gbdate="git branch --format=\"%(authordate:iso) %(refname:lstrip=-1) %(contents:subject)\" | sort"
+alias glods="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --date=short"
+alias l="ls -lah"
