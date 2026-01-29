7d29c26e22d4ca47748faa7610c4404b33789d30
Celso Palmeira Neto <neto@celsoneto.com.br>
2026-01-29 12:03:36 +0000

    feat: install nvim

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 alias gbdate="git branch --format=\"%(authordate:iso) %(refname:lstrip=-1) %(contents:subject)\" | sort"
 alias glods="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --date=short"
 alias l="ls -lah"
--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
+#!/bin/bash
+
+# Function to check if a command is installed, if not install it
+install_if_missing() {
+    local cmd=$1
+    local package=$2
+    
+    if ! command -v $cmd &> /dev/null
+    then
+        echo "$package not found. Installing..."
+        sudo apt-get install -y $package
+    else
+        echo "$package is already installed."
+    fi
+}
+
 sudo apt-get update
 
-# Install btop
-sudo apt-get install btop
+# Check and install neovim
+install_if_missing "nvim" "neovim"
 
+# Check and install btop
+install_if_missing "btop" "btop"
 
