38f06b7055b8485e874b47c81a74a2d75e797d7e
Celso Palmeira Neto <neto@celsoneto.com.br>
2026-07-01 11:41:37 +0100

    feat: enables rerere and uses custom nvim for shares

--- setup_shares.sh
+++ setup_shares.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Check if hostname contains "shares"
-if ! uname -a | grep -q "shares"; then
-    echo "Host is does not contain 'shares' in its name. Exiting."
-    exit 0
-fi
-
 git config --global core.editor "nvim"
+
+git config --global rerere.enabled true
+
+git clone https://github.com/celsopalmeiraneto/kickstart.nvim.git ~/.config/nvim
