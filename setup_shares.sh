303f27d8daf872c5a1e1f7be1f4ea67216a4aee8
Celso Palmeira Neto <neto@celsoneto.com.br>
2026-07-01 12:04:57 +0100

    revert custom nvim

--- setup_shares.sh
+++ setup_shares.sh
@@ -4,9 +4,4 @@ git config --global core.editor "nvim"
 
 git config --global rerere.enabled true
 
-NVIM_CONFIG_DIR=~/.config/nvim
-if [ -d "$NVIM_CONFIG_DIR" ]; then
-  echo "$NVIM_CONFIG_DIR does exist."
-else
-  git clone https://github.com/celsopalmeiraneto/kickstart.nvim.git ~/.config/nvim
-fi
+
